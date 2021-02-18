ed347f616b4629190e7c4b5682914d6e9c6addbd
Steve <pickleman20@hotmail.com>
2021-02-17 20:00:47 -0500

    added a node dependency and adjusted index.js to use requireAuth again

--- src/index.js
+++ src/index.js
@@ -12,16 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false,}));
 app.use("/api", UserRoute );
 
-/*
 app.get('/', requireAuth, (req, res) => {
   res.send(`Your id: ${req.user._id}`);
 });
-*/
-
-app.get('/', function (req, res) {
-  console.log(`Received request from ${req.ip}.`);
-  return res.send(`Howdy, ${req.ip}!`);
-})
 
 let port = process.env.PORT || 3000;
 const server = app.listen(port, function () {
