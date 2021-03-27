ea619f392bd10b20e03b99c54a2b7da4984023e2
Steve <pickleman20@hotmail.com>
2021-03-27 03:44:49 -0400

    Filters now work on backend requests

--- src/Places/places.controller.js
+++ src/Places/places.controller.js
@@ -11,7 +11,25 @@ const infoFields = "name,icon,formatted_address,url,formatted_phone_number,websi
 const getLonLatURL = "http://ip-api.com/json/";
 const getMyIpURL = "http://ipv4bot.whatismyipaddress.com"
 
-function getKeyword(profileInfo) {
+/*
+    {
+        filters: [ '$', 'Under 5 km', '$$', 'Under 10 km', 'Takeout', 'Dine In' ],
+        foodFilters: [ 'Burgers', 'Japanese', 'Healthy', 'Dessert' ]
+    }
+*/
+
+function getKeyword(foodFilters, profileInfo) {
+    if (foodFilters.length > 1) {
+        var foodTypes = `${foodFilters[0]}`
+        for (var index = 1; index < foodFilters.length; index++) {
+            var filterItem = foodFilters[index];
+            foodTypes += ` OR ${filterItem}`;
+        }
+        return `${foodTypes}`;
+    } else if (foodFilters.length == 1) {
+        return `${foodFilters[0]}`
+    }
+
     if (("foodTypes" in profileInfo) && (profileInfo.foodTypes.length > 0)) {
         var foodTypes = profileInfo.foodTypes.join(" OR ");
         return `${foodTypes}`;
@@ -44,20 +62,54 @@ async function getLatLon(ip) {
     }
 }
 
-function getPrice(profileInfo) {
+function getPrice(filters, profileInfo) {
     // Note 0 is least expensive, 4 is most
-    if ("price" in profileInfo) {
+    // Front-end sends '$', '$$', and/or '$$$'
+    var min = 4;
+    var max = 0;
+    for (var index = 0; index < filters.length; index++) {
+        var filterItem = filters[index]
+        if (filterItem == "$") {
+            if (min > 0) min = 0; // If min is higher than 0, lower it to 0
+            if (max < 1) max = 1 // If max is lower than 1, raise it to 1
+        } else if (filterItem == "$$") {
+            if (min > 2) min = 2;
+            if (max < 3) max = 3;
+        } else if (filterItem == "$$$") {
+            if (min > 4) min = 4;
+            if (max < 4) max = 4;
+        }
+    }
+
+    if (min <= max) { // If price has been specified in filters
+        return [min, max]
+    }
+
+    if ("price" in profileInfo) { // If min price has been specified in profile info
         if (profileInfo.price == "none") {
             return undefined;
         } else {
-            return profileInfo.price.length - 1;
+            return [profileInfo.price.length - 1, 4];
         }
     } else {
         return undefined;
     }
 }
 
-function getRadius(profileInfo) {
+function getRadius(filters, profileInfo) {
+    var distance = 50000;
+    for (var index = 0; index < filters.length; index++) {
+        var filterItem = filters[index]
+        if (filterItem == "Under 5 km") {
+            if (distance > 5000) distance = 5000;
+        } else if (filterItem == "Under 10 km") {
+            if (distance > 10000) distance = 10000;
+        } else if (filterItem == "Under 20 km") {
+            if (distance > 20000) distance = 20000;
+        }
+    }
+    return distance;
+    /*
     // Max 50000 meters
     if ("distance" in profileInfo) {
         if (profileInfo.distance > 50) {
@@ -74,9 +126,10 @@ function getRadius(profileInfo) {
     } else {
         return 50000;
     }
+    */
 }
 
-function getParams(location, radius, keyword, minprice) {
+function getParams(location, radius, keyword, price) {
     var params = {};
 
     params.key = process.env.GOOGLE_API_KEY;
@@ -87,9 +140,9 @@ function getParams(location, radius, keyword, minprice) {
     if (keyword != undefined)
         params.keyword = keyword;
 
-    if (minprice != undefined) {
-        params.minprice = minprice;
-        params.maxprice = 4;
+    if (price != undefined) {
+        params.minprice = price[0];
+        params.maxprice = price[1];
     }
 
     return params;
@@ -107,11 +160,20 @@ export async function find(req, res) {
         return res.status(503).send(`Server failed to get a location from Client IP: ${error}`);
     }
 
-    var radius = getRadius(user.profileInfo);
-    var keyword = getKeyword(user.profileInfo)
-    var minprice = getPrice(user.profileInfo);
+    if (!( ("filters" in req.query) || ("foodFilters" in req.query) )) {
+        console.log(`Failed to get a location from Client IP: ${error}`);
+        return res.status(503).send(`Server failed to get a location from Client IP: ${error}`);
+    }
+    
+    // Get filters and foodFilters from query
+    var filters = JSON.parse(req.query.filters);
+    var foodFilters = JSON.parse(req.query.foodFilters);
 
-    var params = getParams(latlon, radius, keyword, minprice);
+    var radius = getRadius(filters, user.profileInfo);
+    var keyword = getKeyword(foodFilters, user.profileInfo)
+    var price = getPrice(filters, user.profileInfo);
+
+    var params = getParams(latlon, radius, keyword, price);
     console.log(params);
 
     var placeDataRequest;
