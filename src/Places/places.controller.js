f3b48a28b0e3b6a02de5e4e6779c91a0e9a51d35
Steve <pickleman20@hotmail.com>
2021-04-02 16:21:54 -0400

    Removed place details and added place photos

--- src/Places/places.controller.js
+++ src/Places/places.controller.js
@@ -8,6 +8,8 @@ const nearbysearchURL = "https://maps.googleapis.com/maps/api/place/nearbysearch
 const getPlaceDetailsURL = "https://maps.googleapis.com/maps/api/place/details";
 const infoFields = "name,icon,formatted_address,url,formatted_phone_number,website";
 
+const getPhotoURL = "https://maps.googleapis.com/maps/api/place/photo";
+
 const getLonLatURL = "http://ip-api.com/json/";
 const getMyIpURL = "http://ipv4bot.whatismyipaddress.com"
 
@@ -123,7 +125,15 @@ export async function find(req, res) {
                     {
                         vicinity: '6 Easton Ave, New Brunswick, NJ 08901, United States',
                         name: 'Vivi Bubble Tea/KBG',
-                        place_id: 'ChIJ_UDANVHGw4kRyF7OUJ3Uxrw'
+                        place_id: 'ChIJ_UDANVHGw4kRyF7OUJ3Uxrw',
+                        photos: [
+                            {
+                                height : 1260,
+                                html_attributions : [ "From a Google User" ],
+                                photo_reference : "CnRwAAAAeM-aLqAm573T44qnNe8bGMkr_BOh1MOVQaA9CCggqtTwuGD1rjsviMyueX_G4-mabgH41Vpr8L27sh-VfZZ8TNCI4FyBiGk0P4fPxjb5Z1LrBZScYzM1glRxR-YjeHd2PWVEqB9cKZB349QqQveJLRIQYKq2PNlOM0toJocR5b_oYRoUYIipdBjMfdUyJN4MZUmhCsTMQwg",
+                                width : 1890
+                            }
+                        ],
                     }
                 ],
                 status: "OK"
@@ -151,11 +161,43 @@ export async function find(req, res) {
     return res.send(JSON.stringify(placeDataRequest.data.results));
 }
 
-// Gets the details of a place
-export async function getInfo(req, res) {
-    var place_id = "PLACE_ID";
-    var url = `${getPlaceDetailsURL}/${outputType}?key=${process.env.GOOGLE_API_KEY}&place_id=${place_id}&fields=${infoFields}`;
-    console.log(req.query);
+// Gets photos given the photo_reference, and maxheight or maxwidth
+export async function getPhoto(req, res) {
+    // Get the photo_reference
+    var photo_reference = req.query.photo_reference;
+    if (photo_reference == undefined) {
+        console.log(`Missing photo_reference`);
+        return res.status(400).send(`Missing photo_reference`);
+    }
 
-    return res.send("ok");
+    // Get maxwidth and/or maxheight
+    var maxwidth = req.query.maxwidth;
+    var maxheight = req.query.maxheight;
+
+    // Get the image url
+    var image_request;
+    try {
+        var params = {
+            key : process.env.GOOGLE_API_KEY,
+            photo_reference : photo_reference,
+            maxheight : maxheight,
+            maxwidth : maxwidth
+        };
+
+        // requests a place with the text query from google maps
+        image_request = await axios({
+            method: "get",
+            responseType: "arraybuffer",
+            url: getPhotoURL,
+            params: params
+        });
+    } catch(error) {
+        console.log(`Status: ${image_request.status}\nFailed to get a photo from Google API: ${error}`);
+        return res.status(503).send(`Server failed to get a photo from Google API: ${error}`);
+    }
+
+    console.log("Sending image");
+    res.set('Content-Type', image_request.headers["content-type"]);
+    res.set('Content-Length', image_request.data.length);
+    res.send(image_request.data);
 }
