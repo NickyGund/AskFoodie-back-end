983362e7f1f175f44617571b8124834fd6d17881
Steve <pickleman20@hotmail.com>
2021-04-24 21:18:49 -0400

    cleaned middleware

--- src/middleware/requireAuth.js
+++ src/middleware/requireAuth.js
@@ -4,24 +4,27 @@ import User from "../User/user.schema.js";
 
 const SECRET = process.env.MY_SECRET;
 
+// Check if the auth token is valid
 export default (req, res, next) => {
-  const { authorization } = req.headers;
-  // authorization === 'Bearer laksjdflaksdjasdfklj'
+    const { authorization } = req.headers;
+    // authorization === 'Bearer laksjdflaksdjasdfklj'
 
-  if (!authorization) {
-    return res.status(401).json({ error: 'You must be logged in.' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  jwt.verify(token, SECRET, async (err, payload) => {
-    if (err) {
-      return res.status(401).json({ error: 'You must be logged in.' });
+    // Checks if the auth token exists
+    if (!authorization) {
+        return res.status(401).json({ error: 'You must be logged in.' });
     }
 
-    const { sub } = payload;
+    // Get the token
+    const token = authorization.replace('Bearer ', '');
+    jwt.verify(token, SECRET, async (err, payload) => {
+        if (err) {
+            return res.status(401).json({ error: 'You must be logged in.' });
+        }
 
-    const user = await User.findById(sub);
-    req.user = user;
-    next();
-  });
+        // If the decrypted token returns the user id, set the user in the request
+        const { sub } = payload;
+        const user = await User.findById(sub);
+        req.user = user;
+        next();
+    });
 };
