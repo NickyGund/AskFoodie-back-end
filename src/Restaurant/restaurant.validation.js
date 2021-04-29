0a5422b75958ebf0dc38fe95e994069660a235be
Steve <pickleman20@hotmail.com>
2021-04-29 17:38:04 -0400

    Fixed missed merge in restaurant validation

--- src/Restaurant/restaurant.validation.js
+++ src/Restaurant/restaurant.validation.js
@@ -2,7 +2,6 @@ import Joi from "@hapi/joi";
 
 // Ensure that data is valid for a restaurant model
 export const addRestaurantValidation = (data) => {
-<<<<<<< HEAD
   const schema = Joi.object({
     place_id: Joi.string().required(),
     name: Joi.string(),
@@ -14,16 +13,3 @@ export const addRestaurantValidation = (data) => {
   });
   return schema.validate(data);
 };
-=======
-    const schema = Joi.object({
-        place_id: Joi.string().required(),
-        name: Joi.string(),
-        address: Joi.string(),
-        phonenumber: Joi.string(),
-        price: Joi.string(),
-        cuisine: Joi.string(),
-        rating: Joi.string(),
-    })
-    return schema.validate(data)
-}
->>>>>>> add_tests_and_cleaning
